328bebc0a50fafa74020e5f143b1ff203642b24d
Lisandra <mara-li@outlook.fr>
2020-07-02 23:43:01 +0200

    easy double gun + comp support/attaque

--- Weapon/competence/cooldown.cc.lua
+++ Weapon/competence/cooldown.cc.lua
@@ -1,24 +1,50 @@
 {{if .CmdArgs}}
-	{{$nom:= (index .CmdArgs 0)}}
-	{{dbSet .User.ID "comp" $nom}}
-	{{$arg:= (dbGet .User.ID "comp").Value}}
-	{{if not (dbGet .User.ID $arg)}}
-		{{dbSet .User.ID $arg 1}}
-		{{dbSet .User.ID "run" (toString .Channel.ID)}}
-		{{ $embed := cembed
-			"description" (joinStr "" "Début du cooldown pour la compétence " $arg)}}
-		{{ $id := sendMessageRetID nil $embed }}
-		{{deleteMessage nil $id 30}}
+	{{if eq (index .CmdArgs 0) "attaque"}}
+		{{$nom:= (index .CmdArgs 1)}}
+		{{dbSet .User.ID "comp" $nom}}
+		{{$arg:= (dbGet .User.ID "comp").Value}}
+		{{if not (dbGet .User.ID $arg)}}
+			{{dbSet .User.ID $arg 1}}
+			{{dbSet .User.ID "run" (toString .Channel.ID)}}
+			{{ $embed := cembed
+				"description" (joinStr "" "Début du cooldown pour la compétence " $arg)}}
+			{{ $id := sendMessageRetID nil $embed }}
+			{{deleteMessage nil $id 30}}
+		{{else}}
+			{{ $embed := cembed
+			"description" (joinStr "" "La compétence " $arg " a déjà été utilisée")}}
+			{{ $id := sendMessageRetID nil $embed }}
+			{{deleteMessage nil $id 30}}
+		{{end}}
+
+	{{else if eq (index .CmdArgs 0) "support"}}
+		{{$nom:= (index .CmdArgs 1)}}
+		{{dbSet .User.ID "aide" $nom}}
+		{{$arg:= (dbGet .User.ID "aide").Value}}
+		{{if not (dbGet .User.ID $arg)}}
+			{{dbSet .User.ID $arg 1}}
+			{{dbSet .User.ID "run" (toString .Channel.ID)}}
+			{{ $embed := cembed
+				"description" (joinStr "" "Début du cooldown pour la compétence " $arg)}}
+			{{ $id := sendMessageRetID nil $embed }}
+			{{deleteMessage nil $id 30}}
+		{{else}}
+			{{ $embed := cembed
+				"description" (joinStr "" "La compétence " $arg " a déjà été utilisée")}}
+			{{ $id := sendMessageRetID nil $embed }}
+			{{deleteMessage nil $id 30}}
+		{{end}}
 	{{else}}
 		{{ $embed := cembed
-		"description" (joinStr "" "La compétence " $arg " a déjà été utilisée")}}
+			"description" "Merci d'indiquer le type de cooldown que vous utilisez.\n\n **Usage** : `$cooldown <attaque|support> nom`"}}
 		{{ $id := sendMessageRetID nil $embed }}
 		{{deleteMessage nil $id 30}}
 	{{end}}
 {{else}}
-	{{ $embed := cembed
-	"description" "Merci d'indiquer la compétence utilisée"}}
-	{{ $id := sendMessageRetID nil $embed }}
-	{{deleteMessage nil $id 30}}
+		{{ $embed := cembed
+		"description" "Merci d'indiquer le type de compétence ainsi que son nom. \n\n **Usage** : `$cooldown <attaque|support> nom`" }}
+		{{ $id := sendMessageRetID nil $embed }}
+		{{deleteMessage nil $id 30}}
 {{end}}
 {{deleteTrigger 1}}
+bite
--- Weapon/competence/countcompt.cc.lua
+++ Weapon/competence/countcompt.cc.lua
@@ -1,16 +1,29 @@
 {{ $matches := (index (reFindAllSubmatches `^\((.*)\)|(^\$(.*))|(^\!\!(.+))|((<a?:[\w~]{2,32}:\d{17,19}>)|[\x{1f1e6}-\x{1f1ff}]{2}|\p{So}\x{fe0f}?[\x{1f3fb}-\x{1f3ff}]?(\x{200D}\p{So}\x{fe0f}?[\x{1f3fb}-\x{1f3ff}]?)*|[#\d*]\x{FE0F}?\x{20E3})` .Message.Content)) }}
 {{$arg := (dbGet .User.ID "comp").Value}}
+{{$arg2 := (dbGet .User.ID "aide").Value}}
+
 {{if not $matches}}
 	{{if (dbGet .User.ID $arg)}}
 		{{if and (lt (dbGet .User.ID $arg).Value (toFloat 3)) (gt (len .Message.Content) 15) }}
 				{{$incr := dbIncr .User.ID $arg 1}}
-			{{else if (eq (dbGet .User.ID $arg).Value (toFloat 3))  }}
+		{{else if (eq (dbGet .User.ID $arg).Value (toFloat 3))  }}
 				{{ $embed := cembed
 					"description" (joinStr "" "Votre compétence " $arg " est de nouveau utilisable")}}
 				{{ $id := sendMessageRetID nil $embed }}
 				{{deleteMessage nil $id 30}}
 				{{dbDel .User.ID $arg}}
 		{{end}}
+
+	{{else if (dbGet .User.ID $arg2)}}
+		{{if and (lt (dbGet .User.ID $arg2).Value (toFloat 6)) (gt (len .Message.Content) 15) }}
+			{{$incr := dbIncr .User.ID $arg2 1}}
+		{{else if (eq (dbGet .User.ID $arg2).Value (toFloat 6))}}
+			{{ $embed := cembed
+				"description" (joinStr "" "Votre compétence " $arg2 " est de nouveau utilisable")}}
+				{{ $id := sendMessageRetID nil $embed }}
+				{{deleteMessage nil $id 30}}
+				{{dbDel .User.ID $arg2}}
+		{{end}}
+	{{else}}
 	{{end}}
-{{else}}
 {{end}}
