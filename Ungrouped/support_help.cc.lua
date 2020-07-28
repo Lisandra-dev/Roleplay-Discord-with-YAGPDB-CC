2293faf2218499216221630fcedb4519a072fdb2
Lisandra <mara-li@outlook.fr>
2020-07-28 20:11:46 +0200

    rename

--- Ungrouped/support_help.cc.lua
+++ Ungrouped/support_help.cc.lua
@@ -1,6 +1,8 @@
+{{$flag := reFind `^\?(malus|soin|poison|support)` .Message.Content}}
+
 {{$id := ""}}
 {{if .CmdArgs}}
-	{{if eq (index .CmdArgs 0) "soin"}}
+	{{if eq $flag "?soin"}}
 		{{$embed := cembed
 			"title" "Soin"
 			"Description" "**0** : URC / Bonus + 40 pv → IMPLANT UNIQUEMENT \n**1** : 40\n**2** : 35\n**3** : 30\n**4** : 25\n**5** : 20\n**6** : 15\n**7** : 10\n**8** : 5\n**9** : Echec\n**10** : Echec + Malus\n\nEn cas d'utilisation avec un pouvoir ou un module :\n:white_small_square: Les PV rendus sont augmentés de 1.\n:white_small_square: Il n'y a pas de malus en cas d'EC."
@@ -8,7 +10,7 @@
 		"thumbnail" (sdict "url" "https://www.sphanalytics.com/wp-content/uploads/2018/01/Health-Icon334.png")}}
       {{$id = sendMessageRetID nil $embed}}
 
-	{{else if eq (index .CmdArgs 0) "malus"}}
+	{{else if eq $flag "?malus"}}
 		{{$embed := cembed
 			"title" "Malus de caractéristiques"
 			"Description" "**0** : +4 → IMPLANT UNIQUEMENT\n**1** : +3\n**2** : +3\n**3** : +2\n**4**: +2\n**5**: +2\n**6**: +1 \n**7**: +1 \n**8**: +1\n**9** : Echec\n**10** : EC → Bonus -2 à l'adversaire"
@@ -16,7 +18,7 @@
 			"thumbnail" (sdict "url" "https://i.imgur.com/fuHvIUn.png")}}
       {{$id = sendMessageRetID nil $embed}}
 
-	{{else if eq (index .CmdArgs 0) "poison"}}
+	{{else if eq $flag "?poison"}}
 		{{$embed := cembed
 			"title" "Empoisonnement"
 			"Description" "Attention, les poisons ne sont pas en pourcentages, et les dégâts sont multipliés, au final, par 3 car une altération dure trois tours. \n\n  **0** : -10 (-30 PV) → IMPLANT UNIQUEMENT \n**1** : -9 (-27PV)\n**2** : -8 (-24 PV)\n**3** : -7 (-21PV)\n**4**: -6 (-18PV)\n**5** : -5 (-15PV)\n**6** : -4 (-12PV) \n**7** : -3 (-9PV)\n**8** : -2 (-6PV)\n**9** : Echec\n**10** : +15 PV (+45PV)"
@@ -25,7 +27,7 @@
       {{$id = sendMessageRetID nil $embed}}
 	{{end}}
 
-{{else}}
+{{else if eq $flag "?support"}}
 {{$embed := cembed
 			"title" "Informations sur les résultats de dés"
 			"fields" (cslice
