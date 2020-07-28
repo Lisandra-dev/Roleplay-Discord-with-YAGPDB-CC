07681a102a819b7c1d374b91780c1bf6756be226
Lisandra <mara-li@outlook.fr>
2020-07-28 17:51:47 +0200

    Comment on peut altéré le karma avec un malus ça n'a pas de sens

--- dice/carac.cc.lua
+++ dice/carac.cc.lua
@@ -1,4 +1,4 @@
-{{$list : = (cslice "la force" "l'endurance" "l'agilité" "la précision" "l'intelligence" "le karma")}}
+{{$list : = (cslice "la force" "l'endurance" "l'agilité" "la précision" "l'intelligence")}}
 {{$c: = (index (shuffle $list) 0)}}
 {{$embed : = cembed
 	"description" (joinStr "" "La caractéristique visée est : " $c ".")
