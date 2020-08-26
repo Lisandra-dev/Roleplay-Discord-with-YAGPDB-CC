9ef052410fb3d084f28937edd74694ea6db2fdb6
Lisandra <mara-li@outlook.fr>
2020-08-26 03:04:21 +0200

    essaie de fix mais pas trouvé :(

--- economy/open_close.go
+++ economy/open_close.go
@@ -19,12 +19,9 @@
 {{if ($serverEco.Get "Items")}}
 	{{$items = sdict ($serverEco.Get "Items")}}
 {{end}}
-{{$name := ""}}
 
 {{if eq $matches "$open"}}
 	{{dbSet .Server.ID "store" "open"}}
-**OUVERTURE**
-
 	{{if $items}}
 		{{range $k, $v := $items}}
 			{{$i := sdict ($items.Get $k)}}
@@ -33,39 +30,39 @@
 			{{$smin := $i.Get "smin"}}
 			{{$smax := $i.Get "smax"}}
 			{{$buyprice := $i.Get "buyprice"}}
+			{{$sellprice := $i.Get "sellprice"}}
 			{{if ne (toInt $bmax) 0}}
 				{{$buyprice = randInt (toInt $bmin) (toInt $bmax)}}
-				{{end}}
-				{{$sellprice := $i.Get "sellprice"}}
-				{{if eq (toInt $smax) 0}}
-					{{$sellprice = "Invendable"}}
-				{{else}}
-					{{$sellprice = randInt (toInt $smin) (toInt $smax)}}
-				{{end}}
-
-				{{$stock := "♾️"}}
-				{{$rstock := randInt 0 100}}
-				{{if le $rstock 10}}
-					{{$stock = 0}}
-				{{else if and (ge $rstock 11) (le $rstock 30)}}
-					{{$stock  = randInt 1 5}}
-				{{end}}
-				{{if eq ($i.Get "rare") "⭐" "⭐⭐" }}
-					{{if le $rstock 10}}
-						{{$stock = randInt 1 10}}
-					{{else if and (ge $rstock 11) (le $rstock 30)}}
-						{{$stock  = randInt 10 20}}
-					{{end}}
-				{{end}}
-				{{$i.Set "buyprice" $buyprice}}
-				{{$i.Set "sellprice" $sellprice}}
-				{{$i.Set "stock" $stock}}
-				{{$items.Set $k $i}}
 			{{end}}
-		{{end}}
+			{{if eq (toInt $smax) 0}}
+				{{$sellprice = "Invendable"}}
+			{{else}}
+				{{$sellprice = randInt (toInt $smin) (toInt $smax)}}
+			{{end}}
 
-		{{$serverEco.Set "Items" $items}}
-		{{dbSet .Server.ID "economy" $serverEco}}
+			{{$stock := "♾️"}}
+			{{$rstock := randInt 0 100}}
+			{{if le $rstock 10}}
+				{{$stock = 0}}
+			{{else if and (ge $rstock 11) (le $rstock 30)}}
+				{{$stock  = randInt 1 5}}
+			{{end}}
+			{{if eq ($i.Get "rare") "⭐" "⭐⭐" }}
+				{{if le $rstock 10}}
+					{{$stock = randInt 1 10}}
+				{{else if and (ge $rstock 11) (le $rstock 30)}}
+					{{$stock  = randInt 10 20}}
+				{{end}}
+			{{end}}
+			{{$i.Set "buyprice" $buyprice}}
+			{{$i.Set "sellprice" $sellprice}}
+			{{$i.Set "stock" $stock}}
+			{{$items.Set $k $i}}
+		{{end}}
+	{{end}}
+
+	{{$serverEco.Set "Items" $items}}
+	{{dbSet .Server.ID "economy" $serverEco}}
 
 	{{if not .CmdArgs}}
 		{{$embed := cembed
@@ -73,15 +70,19 @@
 		"description" $msg_ouvert
 		"color" 0xD5B882}}
 		{{sendMessage $chan $embed}}
+	{{else}}
+		{{$Message := "**Ouverture & Randomisation silencieuse**"}}
+		{{sendMessage nil $Message}}
 	{{end}}
 
-	{{else}}
+{{else}}
 	**FERMÉ**
-		{{dbSet .Server.ID "store" "close"}}
-		{{$embed := cembed
-		"author" (sdict "name" $author "icon_url" $avatar)
-		"description" $msg_close
-		"color" 0xD5B882}}
-		{{sendMessage $chan $embed}}
-	{{end}}
+	{{dbSet .Server.ID "store" "close"}}
+	{{$embed := cembed
+	"author" (sdict "name" $author "icon_url" $avatar)
+	"description" $msg_close
+	"color" 0xD5B882}}
+	{{sendMessage $chan $embed}}
+{{end}}
+
 {{deleteTrigger 1}}
