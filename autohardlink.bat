ff54bef714f7ca5eea15c3a62d4425796db23338
Mara <mara-li@outlook.fr>
2020-09-12 23:39:43 +0200

    update lieux fichier

--- autohardlink.bat
+++ autohardlink.bat
@@ -1,8 +1,8 @@
 @ECHO ON
 
-SET SrcRoot=C:\Users\Lili\Documents\GitHub\YAGPDB-cc-rp.wiki
+SET SrcRoot=C:\Users\Lili\Documents\GitHub\YAGPDB-cc-rp\documentations
 SET TargetRoot=C:\Users\Lili\Documents\GitHub\Projet-Nucleus-Custom-Command.wiki
-SET RepoRoot=C:\Users\Lili\Documents\GitHub\YAGPDB-cc-rp\documentations
+SET RepoRoot=C:\Users\Lili\Documents\GitHub\YAGPDB-cc-rp.wiki
 
 FOR /D %%A IN ("%SrcRoot%\*") DO (
     MKLINK /D "%TargetRoot%\%%~NA" "%%~A"
